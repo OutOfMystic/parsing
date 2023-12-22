4d6cd6f4a628396895528a396bf3f99c860dcaeb
mikheyev <mikheyev9@gmail.com>
2023-12-22 16:29:57 +0300

    add default headers

--- parse_module/manager/proxy/check.py
+++ parse_module/manager/proxy/check.py
@@ -24,7 +24,24 @@ class SpecialConditions:
 
     @staticmethod
     def default_handler(proxy, url, method):
-        r = method(url, proxies=proxy.requests, timeout=10)
+        from ...models import user_agent
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp'
+                      ',image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'no-cache',
+            'pragma': 'no-cache',
+            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': user_agent.random()
+        }
+        r = method(url, proxies=proxy.requests, headers=headers, timeout=10)
         if not r.ok:
             raise ProxyError
 
