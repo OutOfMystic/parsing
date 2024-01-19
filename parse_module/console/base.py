39d6fcbc1ed4435c279f8fca3b2cd2dfa2d8b5ce
OutOfMystic <vi3sa@ya.ru>
2024-01-19 14:10:31 -0300

    console loggng

--- parse_module/console/base.py
+++ parse_module/console/base.py
@@ -18,7 +18,7 @@ class CommandPrompt:
         args_row = args_row.strip()
         result = provision.multi_try(self.handler, name='Command', tries=1,
                                      args=(cmd, args_row, self.value),
-                                     raise_exc=False, log_func=None)
+                                     raise_exc=False)
         if result is None:
             handler, value = None, None
         elif result == provision.TryError:
