6f8d48e3f657ae230de4801a7f66c872866bf584
mikheyev <mikheyev9@gmail.com>
2024-02-19 17:21:09 +0300

    on gubernskii

--- working_directory/parsers/afisha_ru_events.py
+++ working_directory/parsers/afisha_ru_events.py
@@ -36,7 +36,7 @@ class AfishaEvents(AsyncEventParser):
             #'https://www.afisha.ru/msk/theatre/teatr-gogolya-15926286/': '*', #gogolia theatr ##
             #'https://www.afisha.ru/msk/concerthall/crocus-city-hall-5222/': '*', # крокус сити холл ##
             'https://www.afisha.ru/msk/theatre/teatr-rossiyskoy-armii-15877731/': '*', #armii
-            #'https://www.afisha.ru/msk/theatre/gubernskiy-teatr-15883628/': '*', #gubernskiy
+            'https://www.afisha.ru/msk/theatre/gubernskiy-teatr-15883628/': '*', #gubernskiy
         }
         self.ssl_context = ssl.create_default_context()
         self.ssl_context.check_hostname = False
